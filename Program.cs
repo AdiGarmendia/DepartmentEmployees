f39f437ed295331012b1f309b33c471e843b5009
AdiGarmendia <dejima@gmail.com>
2020-03-20 15:47:00 -0500

    test

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using DepartmentsEmployees.Data;
+using DepartmentsEmployees.Models;
+using System;
+
+namespace DepartmentsEmployees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DepartmentRepository departmentRepo = new DepartmentRepository();
+
+            Console.WriteLine("Getting All Departments:");
+            Console.WriteLine();
+
+            System.Collections.Generic.List<Department> allDepartments = departmentRepo.GetAllDepartments();
+
+            foreach (Department dept in allDepartments)
+            {
+                Console.WriteLine($"{dept.Id} {dept.DeptName}");
+            }
+        }
+    }
+}
