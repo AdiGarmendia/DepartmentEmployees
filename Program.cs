e32cc85f31dd9524164d187352812e664783d4f4
AdiGarmendia <dejima@gmail.com>
2020-03-23 15:52:07 -0500

    employee work, breaks in the middle

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using DepartmentsEmployees.Data;
 using DepartmentsEmployees.Models;
+using DepartmentsEmployeesConsole.Data;
 using System;
 using System.Collections.Generic;
 
@@ -37,6 +38,37 @@ namespace DepartmentsEmployees
 
             Console.WriteLine("-------------------------------");
             Console.WriteLine("Added the new Legal Department!");
+        
+        
+            var repo = new EmployeeRepository();
+        //var employees = repo.GetAllEmployees();
+
+        //foreach (var employee in employees)
+        //{
+        //    Console.WriteLine($"{employee.FirstName} {employee.LastName} is in {employee.Department.DeptName}");
+        //}
+
+
+        //Console.WriteLine("Let's get an employee with the ID 2");
+
+        //var employeeWithId2 = repo.GetEmployeeById(2);
+
+        //Console.WriteLine($"Employee with Id 2 is {employeeWithId2.FirstName} {employeeWithId2.LastName}");
+
+        Console.WriteLine("First Name");
+            var firstName = Console.ReadLine();
+
+        Console.WriteLine("Last Name");
+            var lastName = Console.ReadLine();
+
+        var newEmployee = new Employee()
+        {
+            FirstName = firstName,
+            LastName = lastName,
+            DepartmentId = 2
+        };
+
+        repo.CreateNewEmployee(newEmployee);
         }
-    }
+}
 }
